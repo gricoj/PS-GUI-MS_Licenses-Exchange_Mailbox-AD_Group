8bc5a3d4c1fcd681814ec5a70a9a793a817fb4dd
George Rico Juarez <georgerico417@gmail.com>
2019-11-27 11:02:40 -0600

    Update gui.ps1

--- gui.ps1
+++ gui.ps1
@@ -7,87 +7,87 @@ $Form.text                       = "Form"
 $Form.BackColor                  = "#4a4a4a"
 $Form.TopMost                    = $false
 
-$TextBox1                        = New-Object system.Windows.Forms.TextBox
-$TextBox1.multiline              = $false
-$TextBox1.text                   = "Username"
-$TextBox1.BackColor              = "#777773"
-$TextBox1.width                  = 132
-$TextBox1.height                 = 20
-$TextBox1.location               = New-Object System.Drawing.Point(14,30)
-$TextBox1.Font                   = 'Microsoft Sans Serif,10'
-$TextBox1.ForeColor                = "#ffffff"
+$Search_Box                        = New-Object system.Windows.Forms.TextBox
+$Search_Box.multiline              = $false
+$Search_Box.text                   = "Username"
+$Search_Box.BackColor              = "#777773"
+$Search_Box.width                  = 132
+$Search_Box.height                 = 20
+$Search_Box.location               = New-Object System.Drawing.Point(14,30)
+$Search_Box.Font                   = 'Microsoft Sans Serif,10'
+$Search_Box.ForeColor                = "#ffffff"
 
-$Button1                         = New-Object system.Windows.Forms.Button
-$Button1.text                    = "Search"
-$Button1.width                   = 60
-$Button1.height                  = 30
-$Button1.location                = New-Object System.Drawing.Point(160,25)
-$Button1.Font                    = 'Microsoft Sans Serif,10'
-$Button1.ForeColor                = "#ffffff"
+$Search_Button                         = New-Object system.Windows.Forms.Button
+$Search_Button.text                    = "Search"
+$Search_Button.width                   = 60
+$Search_Button.height                  = 30
+$Search_Button.location                = New-Object System.Drawing.Point(160,25)
+$Search_Button.Font                    = 'Microsoft Sans Serif,10'
+$Search_Button.ForeColor                = "#ffffff"
 
-$TextBox2                        = New-Object system.Windows.Forms.TextBox
-$TextBox2.multiline              = $false
-$TextBox2.BackColor              = "#777773"
-$TextBox2.width                  = 210
-$TextBox2.height                 = 20
-$TextBox2.location               = New-Object System.Drawing.Point(154,76)
-$TextBox2.Font                   = 'Microsoft Sans Serif,10'
-$TextBox2.ForeColor                = "#ffffff"
+$ExchangeStatus_Box                        = New-Object system.Windows.Forms.TextBox
+$ExchangeStatus_Box.multiline              = $false
+$ExchangeStatus_Box.BackColor              = "#777773"
+$ExchangeStatus_Box.width                  = 210
+$ExchangeStatus_Box.height                 = 20
+$ExchangeStatus_Box.location               = New-Object System.Drawing.Point(154,76)
+$ExchangeStatus_Box.Font                   = 'Microsoft Sans Serif,10'
+$ExchangeStatus_Box.ForeColor                = "#ffffff"
 
-$Label1                          = New-Object system.Windows.Forms.Label
-$Label1.text                     = "Exchange Status"
-$Label1.AutoSize                 = $true
-$Label1.width                    = 25
-$Label1.height                   = 10
-$Label1.location                 = New-Object System.Drawing.Point(30,76)
-$Label1.Font                     = 'Microsoft Sans Serif,10'
-$Label1.ForeColor                = "#ffffff"
+$ExchangeStatus_Label                          = New-Object system.Windows.Forms.Label
+$ExchangeStatus_Label.text                     = "Exchange Status"
+$ExchangeStatus_Label.AutoSize                 = $true
+$ExchangeStatus_Label.width                    = 25
+$ExchangeStatus_Label.height                   = 10
+$ExchangeStatus_Label.location                 = New-Object System.Drawing.Point(30,76)
+$ExchangeStatus_Label.Font                     = 'Microsoft Sans Serif,10'
+$ExchangeStatus_Label.ForeColor                = "#ffffff"
 
-$TextBox3                        = New-Object system.Windows.Forms.TextBox
-$TextBox3.multiline              = $true
-$TextBox3.BackColor              = "#777773"
-$TextBox3.width                  = 210
-$TextBox3.height                 = 74
-$TextBox3.location               = New-Object System.Drawing.Point(154,105)
-$TextBox3.Font                   = 'Microsoft Sans Serif,10'
-$TextBox3.ForeColor                = "#ffffff"
+$LicenseDetail_Box                        = New-Object system.Windows.Forms.TextBox
+$LicenseDetail_Box.multiline              = $true
+$LicenseDetail_Box.BackColor              = "#777773"
+$LicenseDetail_Box.width                  = 210
+$LicenseDetail_Box.height                 = 74
+$LicenseDetail_Box.location               = New-Object System.Drawing.Point(154,105)
+$LicenseDetail_Box.Font                   = 'Microsoft Sans Serif,10'
+$LicenseDetail_Box.ForeColor                = "#ffffff"
 
-$Label2                          = New-Object system.Windows.Forms.Label
-$Label2.text                     = "License Detail"
-$Label2.AutoSize                 = $true
-$Label2.width                    = 25
-$Label2.height                   = 10
-$Label2.location                 = New-Object System.Drawing.Point(37,130)
-$Label2.Font                     = 'Microsoft Sans Serif,10'
-$Label2.ForeColor                = "#ffffff"
+$LicenseDetail_Label                          = New-Object system.Windows.Forms.Label
+$LicenseDetail_Label.text                     = "License Detail"
+$LicenseDetail_Label.AutoSize                 = $true
+$LicenseDetail_Label.width                    = 25
+$LicenseDetail_Label.height                   = 10
+$LicenseDetail_Label.location                 = New-Object System.Drawing.Point(37,130)
+$LicenseDetail_Label.Font                     = 'Microsoft Sans Serif,10'
+$LicenseDetail_Label.ForeColor                = "#ffffff"
 
-$TextBox4                        = New-Object system.Windows.Forms.TextBox
-$TextBox4.multiline              = $true
-$TextBox4.BackColor              = "#777773"
-$TextBox4.width                  = 70
-$TextBox4.height                 = 20
-$TextBox4.location               = New-Object System.Drawing.Point(194,185)
-$TextBox4.Font                   = 'Microsoft Sans Serif,10'
-$TextBox4.ForeColor                = "#ffffff"
+$InADGroup_Box                        = New-Object system.Windows.Forms.TextBox
+$InADGroup_Box.multiline              = $true
+$InADGroup_Box.BackColor              = "#777773"
+$InADGroup_Box.width                  = 70
+$InADGroup_Box.height                 = 20
+$InADGroup_Box.location               = New-Object System.Drawing.Point(194,185)
+$InADGroup_Box.Font                   = 'Microsoft Sans Serif,10'
+$InADGroup_Box.ForeColor                = "#ffffff"
 
-$Label3                          = New-Object system.Windows.Forms.Label
-$Label3.text                     = "In MDM_OnPremExchange"
-$Label3.AutoSize                 = $true
-$Label3.width                    = 25
-$Label3.height                   = 10
-$Label3.location                 = New-Object System.Drawing.Point(20,185)
-$Label3.Font                     = 'Microsoft Sans Serif,10'
-$Label3.ForeColor                = "#ffffff"
+$InADGroup_Label                          = New-Object system.Windows.Forms.Label
+$InADGroup_Label.text                     = "In MDM_OnPremExchange"
+$InADGroup_Label.AutoSize                 = $true
+$InADGroup_Label.width                    = 25
+$InADGroup_Label.height                   = 10
+$InADGroup_Label.location                 = New-Object System.Drawing.Point(20,185)
+$InADGroup_Label.Font                     = 'Microsoft Sans Serif,10'
+$InADGroup_Label.ForeColor                = "#ffffff"
 
-$Button2                         = New-Object system.Windows.Forms.Button
-$Button2.width                   = 60
-$Button2.height                  = 20
-$Button2.location                = New-Object System.Drawing.Point(270,185)
-$Button2.Font                    = 'Microsoft Sans Serif,8'
-$Button2.ForeColor                = "#ffffff"
-$Button2.visible                 = $false
+$AddRemove_Button                         = New-Object system.Windows.Forms.Button
+$AddRemove_Button.width                   = 60
+$AddRemove_Button.height                  = 20
+$AddRemove_Button.location                = New-Object System.Drawing.Point(270,185)
+$AddRemove_Button.Font                    = 'Microsoft Sans Serif,8'
+$AddRemove_Button.ForeColor                = "#ffffff"
+$AddRemove_Button.visible                 = $false
 
-$Form.controls.AddRange(@($TextBox1,$Button1,$Button2,$TextBox2,$Label1,$TextBox3,$Label2,$TextBox4,$Label3))
+$Form.controls.AddRange(@($Search_Box,$Search_Button,$AddRemove_Button,$ExchangeStatus_Box,$ExchangeStatus_Label,$LicenseDetail_Box,$LicenseDetail_Label,$InADGroup_Box,$InADGroup_Label))
 
 function Get-ExchangeStatus {
     param([string]$Username)
@@ -115,39 +115,39 @@ function In-OnPremGroup {
     return $InGroup
 }
 
-$TextBox1.Add_Click({ $TextBox1.Clear()})
+$Search_Box.Add_Click({ $Search_Box.Clear()})
 
-$Button1.Add_Click({
-    $Button2.visible = $false
-    $TextBox2.Clear()
-    $TextBox3.Clear()
-    $TextBox4.Clear()
-    if($TextBox1.Text -eq "" -or $TextBox1.Text -eq "Username"){ return }
+$Search_Button.Add_Click({
+    $AddRemove_Button.visible = $false
+    $ExchangeStatus_Box.Clear()
+    $LicenseDetail_Box.Clear()
+    $InADGroup_Box.Clear()
+    if($Search_Box.Text -eq "" -or $Search_Box.Text -eq "Username"){ return }
 
-    $UserName = $TextBox1.Text
-    $TextBox2.Text = Get-ExchangeStatus -Username $UserName
+    $UserName = $Search_Box.Text
+    $ExchangeStatus_Box.Text = Get-ExchangeStatus -Username $UserName
     
     $UPN = (Get-ADUser $UserName).UserPrincipalName
     $Licenses = (Get-UserLicenseDetail -UserPrincipalName $UPN)
 
     foreach($License in $Licenses){
-        $TextBox3.AppendText($License)
-        $TextBox3.AppendText("`n")
+        $LicenseDetail_Box.AppendText($License)
+        $LicenseDetail_Box.AppendText("`n")
     }
 
-    $TextBox4.Text = In-OnPremGroup -Username $UserName
+    $InADGroup_Box.Text = In-OnPremGroup -Username $UserName
 
-    if($TextBox4.Text -eq $false -and $TextBox2.Text -eq "On Premise" -and $TextBox3.lines -contains "EMS" ){$Button2.text = "Add"; $Button2.visible = $true; }
-    elseif($TextBox4.Text -eq $true -and $TextBox2.Text -eq "O365"){ $Button2.text = "Remove"; $Button2.visible = $true;}
+    if($InADGroup_Box.Text -eq $false -and $ExchangeStatus_Box.Text -eq "On Premise" -and $LicenseDetail_Box.lines -contains "EMS" ){$AddRemove_Button.text = "Add"; $AddRemove_Button.visible = $true; }
+    elseif($InADGroup_Box.Text -eq $true -and $ExchangeStatus_Box.Text -eq "O365"){ $AddRemove_Button.text = "Remove"; $AddRemove_Button.visible = $true;}
         
 })
 
-$Button2.Add_Click({
-    if($Button2.text -eq "Remove"){Remove-ADGroupMember -Identity MDM_OnPremExchange -Members $TextBox1.Text}
-    elseif($Button2.text -eq "Add"){Add-ADGroupMember -Identity MDM_OnPremExchange -Members $TextBox1.Text}
+$AddRemove_Button.Add_Click({
+    if($AddRemove_Button.text -eq "Remove"){Remove-ADGroupMember -Identity MDM_OnPremExchange -Members $Search_Box.Text}
+    elseif($AddRemove_Button.text -eq "Add"){Add-ADGroupMember -Identity MDM_OnPremExchange -Members $Search_Box.Text}
     Start-Sleep -s 3
-    $TextBox4.Text = In-OnPremGroup -UserPrincipalName $TextBox1.Text
-    $Button2.visible = $false
+    $InADGroup_Box.Text = In-OnPremGroup -UserPrincipalName $Search_Box.Text
+    $AddRemove_Button.visible = $false
 })   
 
 $Form.ShowDialog()
